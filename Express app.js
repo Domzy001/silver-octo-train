2a89d6750def22230e159b8efc7e8fedf5178969
Domzy001 <dadeb25@gmail.com>
2025-08-20 14:20:45 +0100

    Update Express app.js

--- Express app.js
+++ Express app.js
@@ -7,3 +7,13 @@ app.use(express.static(path.join(__dirname, 'frontend/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
 });
+
+const path = require('path');
+
+// Serve static frontend build
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+// Catch-all route for React Router
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
+});
